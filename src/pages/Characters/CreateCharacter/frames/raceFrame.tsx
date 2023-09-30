fd6afc044fec594ac63ac2349ff51bebaa384361
Žaneta Trošková <troskzan@fit.cvut.cz>
2023-09-30 20:09:01 +0200

    add logic with visibility in race frame

--- src/pages/Characters/CreateCharacter/frames/raceFrame.tsx
+++ src/pages/Characters/CreateCharacter/frames/raceFrame.tsx
@@ -9,6 +9,13 @@ import MenuItem from "@mui/material/MenuItem";
 import InputLabel from "@mui/material/InputLabel";
 import FormControl from "@mui/material/FormControl";
 import Select, { SelectChangeEvent } from "@mui/material/Select";
+import { useState, useEffect } from "react";
+
+//interface used for autocomplete
+interface AutocompleteItem {
+  id: number;
+  title: string;
+}
 const sizes = [
   {
     value: "M",
@@ -19,11 +26,6 @@ const sizes = [
     label: "Small",
   },
 ];
-//interface used for autocomplete
-interface AutocompleteItem {
-  id: number;
-  title: string;
-}
 const languages: AutocompleteItem[] = [
   // Elvish languages
   { id: generateId(), title: "Common Elvish" },
@@ -75,7 +77,10 @@ const languages: AutocompleteItem[] = [
 function generateId(): number {
   return Math.random();
 }
-const races = [
+interface Race{
+  label: string
+}
+const races: Race[] = [
   { label: "Centaur" },
   { label: "Dragonborn" },
   { label: "Elf (High)" },
@@ -83,10 +88,18 @@ const races = [
   { label: "Human" },
 ];
 export default function Race() {
-  const [size, setSize] = React.useState("M");
+  const [size, setSize] = useState("M");
+  const [race, setRace] = useState<Race| null>(null);
+  const [isVisible, setVisibility] = React.useState(false);
   function handleChange(event: SelectChangeEvent) {
     setSize(event.target.value);
   }
+  const onChange = (e: HTMLInputElement, value: string) => {
+
+  }
+  // useEffect(() => {
+  //   if( )
+  // }, race)
   return (
     <Box sx={{ pt: 2, pb: 3 }}>
       <Grid container sx={{ py: 2 }}>
@@ -95,7 +108,16 @@ export default function Race() {
             disablePortal
             id="combo-box-demo"
             options={races}
+            value={race}
             sx={{ m: 2 }}
+            onChange={(_, value: Race | null) => {
+              if( !value ) return;
+              setRace(value);
+              console.log('reeeeeeee', race);
+              if( race?.label !== "" ) setVisibility(true); 
+              else setVisibility(false);
+
+            }}
             renderInput={(params) => (
               <TextField
                 {...params}
@@ -107,98 +129,119 @@ export default function Race() {
           />
         </Grid>
       </Grid>
-      <Box>
-        <Divider sx={{ p: 2 }}>
-          <Typography variant="overline" display="block" gutterBottom>
-            further information
-          </Typography>
-        </Divider>
-      </Box>
-      <Grid container sx={{ py: 2 }}>
-        <Grid item lg={6} xs={12} sx={{ p: 2 }}>
-          <MultiComplete
-            values={languages}
-            defaultValue={languages[0]}
-            label="Languages"
-            helpText="Please choose 3 languages"
-            placeholder="elsiftisna"
-            maxItems={3}
-          />
-        </Grid>
-        <Grid item lg={4} xs={12} sx={{ p: 2, pl: 7 }}>
-          <FormControl variant="filled" fullWidth>
-            <InputLabel id="demo-simple-select-label">Size</InputLabel>
-            <Select
-              labelId="demo-simple-select-label"
-              id="demo-simple-select"
-              value={size}
-              label="Size"
-              defaultValue={size}
-              onChange={handleChange}
-            >
-              {sizes.map((option) => (
-                <MenuItem key={option.value} value={option.value}>
-                  {option.label}
-                </MenuItem>
-              ))}
-            </Select>
-          </FormControl>
-        </Grid>
-      </Grid>
-      <Box display="flex" flexGrow={1}>
-        <Card
-          sx={{
-            display: "flex",
-            m: 2,
-            p: 1,
-            justifyContent: "flex-start",
-            flexDirection: "column",
-          }}
-        >
-          <CardContent sx={{ display: "flex", pb: 0 }}>
-            <Typography gutterBottom variant="h5" component="div" align="left">
-              High Elf
-            </Typography>
-          </CardContent>
-          <CardContent
-            sx={{
-              display: "flex",
-              py: 0,
-            }}
-          >
-            <Typography
-              gutterBottom
-              variant="body1"
-              component="div"
-              align="left"
-              sx={{ pr: 1 }}
-            >
-              Speed:
-            </Typography>
-            <Typography variant="body1" color="text.secondary" align="justify">
-              20 km/h
-            </Typography>
-          </CardContent>
-          <CardContent
-            sx={{
-              display: "flex",
-            }}
-          >
-            <Typography variant="body1" color="text.secondary" align="justify">
-              As a high elf, you have a keen mind and a mastery of at least the
-              basics of magic. In many of the worlds of D&D, there are two kinds
-              of high elves. One type (which includes the gray elves and valley
-              elves of Greyhawk, the Silvanesti of Dragonlance, and the sun
-              elves of the Forgotten Realms) is haughty and reclusive, believing
-              themselves to be superior to non-elves and even other elves. The
-              other type (including the high elves of Greyhawk, the Qualinesti
-              of Dragonlance, and the moon elves of the Forgotten Realms) are
-              more common and more friendly, and often encountered among humans
-              and other races.
-            </Typography>
-          </CardContent>
-        </Card>
-      </Box>
+      <React.Fragment>
+        {isVisible && (
+          <div>
+            <Box>
+              <Divider sx={{ p: 2 }}>
+                <Typography variant="overline" display="block" gutterBottom>
+                  further information
+                </Typography>
+              </Divider>
+            </Box>
+            <Grid container sx={{ py: 2 }}>
+              <Grid item lg={6} xs={12} sx={{ p: 2 }}>
+                <MultiComplete
+                  values={languages}
+                  defaultValue={languages[0]}
+                  label="Languages"
+                  helpText="Please choose 3 languages"
+                  placeholder="elsiftisna"
+                  maxItems={3}
+                />
+              </Grid>
+
+              <Grid item lg={4} xs={12} sx={{ p: 2, pl: 7 }}>
+                <FormControl variant="filled" fullWidth>
+                  <InputLabel id="demo-simple-select-label">Size</InputLabel>
+                  <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    value={size}
+                    label="Size"
+                    defaultValue={size}
+                    onChange={handleChange}
+                  >
+                    {sizes.map((option) => (
+                      <MenuItem key={option.value} value={option.value}>
+                        {option.label}
+                      </MenuItem>
+                    ))}
+                  </Select>
+                </FormControl>
+              </Grid>
+            </Grid>
+            <Box display="flex" flexGrow={1}>
+              <Card
+                sx={{
+                  display: "flex",
+                  m: 2,
+                  p: 1,
+                  justifyContent: "flex-start",
+                  flexDirection: "column",
+                }}
+              >
+                <CardContent sx={{ display: "flex", pb: 0 }}>
+                  <Typography
+                    gutterBottom
+                    variant="h5"
+                    component="div"
+                    align="left"
+                  >
+                    High Elf
+                  </Typography>
+                </CardContent>
+                <CardContent
+                  sx={{
+                    display: "flex",
+                    py: 0,
+                  }}
+                >
+                  <Typography
+                    gutterBottom
+                    variant="body1"
+                    component="div"
+                    align="left"
+                    sx={{ pr: 1 }}
+                  >
+                    Speed:
+                  </Typography>
+                  <Typography
+                    variant="body1"
+                    color="text.secondary"
+                    align="justify"
+                  >
+                    20 km/h
+                  </Typography>
+                </CardContent>
+                <CardContent
+                  sx={{
+                    display: "flex",
+                  }}
+                >
+                  <Typography
+                    variant="body1"
+                    color="text.secondary"
+                    align="justify"
+                  >
+                    As a high elf, you have a keen mind and a mastery of at
+                    least the basics of magic. In many of the worlds of D&D,
+                    there are two kinds of high elves. One type (which includes
+                    the gray elves and valley elves of Greyhawk, the Silvanesti
+                    of Dragonlance, and the sun elves of the Forgotten Realms)
+                    is haughty and reclusive, believing themselves to be
+                    superior to non-elves and even other elves. The other type
+                    (including the high elves of Greyhawk, the Qualinesti of
+                    Dragonlance, and the moon elves of the Forgotten Realms) are
+                    more common and more friendly, and often encountered among
+                    humans and other races.
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Box>
+          </div>
+        )}
+      </React.Fragment>
     </Box>
   );
 }
