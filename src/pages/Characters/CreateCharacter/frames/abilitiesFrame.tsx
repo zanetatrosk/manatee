5721d135ac4f38766bddebb3d1aa34ad5b192f63
Žaneta Trošková <troskzan@fit.cvut.cz>
2023-10-14 00:11:35 +0200

    setting constants in ability and changing range in Score input

--- src/pages/Characters/CreateCharacter/frames/abilitiesFrame.tsx
+++ src/pages/Characters/CreateCharacter/frames/abilitiesFrame.tsx
@@ -5,28 +5,28 @@ import TableCell from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
-import Card from "@mui/material/Card"
+import Card from "@mui/material/Card";
 import Checkbox from "@mui/material/Checkbox";
 import { styled } from "@mui/material/styles";
 import { tableCellClasses } from "@mui/material/TableCell";
 import { TextField, Typography, CardContent } from "@mui/material";
-function createData(
-  name: string,
-  calories: number,
-  fat: number,
-  carbs: number,
-  protein: number
-) {
-  return { name, calories, fat, carbs, protein };
+import { Ability } from "@pages/Characters/definitions/characterForm";
+
+const MIN = 1;
+const MAX = 20;
+const BASE_10 = 10;
+const DEFAULT_SCORE = 8;
+function createData(name: Ability, modifier: number) {
+  return { name, modifier };
 }
 
 const rows = [
-  createData("Strength", 159, 6.0, 24, 4.0),
-  createData("Dexterity", 237, 9.0, 37, 4.3),
-  createData("constitution", 262, 16.0, 24, 6.0),
-  createData("Intelligence", 305, 3.7, 67, 4.3),
-  createData("wisdom", 356, 16.0, 49, 3.9),
-  createData("charisma", 356, 16.0, 49, 3.9),
+  createData(Ability.STRENGTH, -1),
+  createData(Ability.DEXTERITY, -1),
+  createData(Ability.CONSTITUTION, -1),
+  createData(Ability.INTELLIGENCE, -1),
+  createData(Ability.WISDOM, -1),
+  createData(Ability.CHARISMA, -1),
 ];
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
@@ -50,6 +50,7 @@ const StyledModifier = styled(TableCell)(({ theme }) => ({
 }));
 
 export default function Abilities() {
+  const [scores, setScores] = React.useState<number[]>([]);
   return (
     <TableContainer component={Card}>
       <CardContent>
@@ -57,7 +58,7 @@ export default function Abilities() {
           Abilities
         </Typography>
         <Typography variant="body2" color="text.secondary">
-        Additional description if required
+          Additional description if required
         </Typography>
       </CardContent>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
@@ -71,22 +72,32 @@ export default function Abilities() {
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
+          {rows.map((row, idx) => (
             <TableRow
               key={row.name}
               sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
             >
-              <StyledTableCell >{row.name}</StyledTableCell>
-              <TableCell align="left" sx={{ maxWidth:151}}>
+              <StyledTableCell>{row.name}</StyledTableCell>
+              <TableCell align="left" sx={{ maxWidth: 151 }}>
                 <TextField
                   id="outlined-number"
                   type="number"
+                  value={scores[idx] || DEFAULT_SCORE}
+                  inputProps={{ min: MIN, max: MAX }}
+                  onChange={(e) => {
+                    var value = parseInt(e.target.value, BASE_10);
+                    if (value > MAX) value = MAX;
+                    if (value < MIN) value = MIN;
+                    let newValues = [...scores];
+                    newValues[idx] = value;
+                    setScores(newValues);
+                  }}
                   InputLabelProps={{
                     shrink: true,
                   }}
                 />
               </TableCell>
-              <StyledModifier align="left">{row.fat}</StyledModifier>
+              <StyledModifier align="left">{row.modifier}</StyledModifier>
               <TableCell>
                 <Checkbox color="primary" />
               </TableCell>
