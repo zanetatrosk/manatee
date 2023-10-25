27e67f288873190e3e2557e862a8acef20494ab7
Žaneta Trošková <troskzan@fit.cvut.cz>
2023-10-26 00:38:09 +0200

    wrapping the component statsGrid

--- src/pages/homePage.tsx
+++ src/pages/homePage.tsx
@@ -1,45 +1,67 @@
 import { Card, CardContent, Grid, Paper, Typography } from "@mui/material";
 import Box from "@mui/material/Box";
+import React from "react";
+import StatsGrid from "./CharacterSheet/components/statsGrid";
 
-function LittleCard() {
-  return (
-    <Paper
-      elevation={12}
-      sx={{ borderRadius: 1, px: 3, py: 1, pb: 0.3, width: 128 }}
-    >
-      <Grid container flexDirection="column" justifyItems="center">
-        <Grid item container justifyContent="center">
-          <Typography variant="button" gutterBottom>
-            Speed
-          </Typography>
-        </Grid>
-        <Grid item>
-          <Typography variant="h4">35 ft</Typography>
-        </Grid>
-      </Grid>
-    </Paper>
-  );
+interface Item {
+  header: string;
+  value: string;
+}
+interface Props {
+  title: string;
+  items: Item[];
 }
 
 export default function HomePage() {
   return (
     <Box display="flex" justifyContent="center">
-      <Card sx={{ p: 4, display: "flex", maxWidth: 600 }}>
-        <Grid container flexDirection={"column"} spacing={3}>
-          <Grid item>
-            <Typography variant="h5" component="div">
-              Home Page
-            </Typography>
-          </Grid>
-            <Grid container item spacing={3} alignItems={"center"}>
-              {Array.from(Array(6).keys()).map((i) => (
-                <Grid item container xs key={i} justifyContent={"center"}>
-                  <LittleCard />
-                </Grid>
-              ))}
-            </Grid>
-          </Grid>
-      </Card>
+      <StatsGrid title="aaa" items={items} />
     </Box>
   );
 }
+const items: Item[] = [
+  {
+    header: "speed",
+    value: "35 ft",
+  },
+  {
+    header: "Initiative",
+    value: "+2",
+  },
+  {
+    header: "Prof. Bonus",
+    value: "+2",
+  },
+  {
+    header: "Armor Class",
+    value: "15",
+  },
+  {
+    header: "Hit Points max",
+    value: "412",
+  },
+  {
+    header: "Hit Dice",
+    value: "1d10",
+  },
+  {
+    header: "Hit Dice",
+    value: "1d10",
+  },
+  {
+    header: "Hit Dice",
+    value: "1d10",
+  },
+  {
+    header: "Hit Dice",
+    value: "1d10",
+  },
+  {
+    header: "Hit Dice",
+    value: "1d10",
+  },
+  {
+    header: "Hit Dice",
+    value: "1d10",
+  },
+];
