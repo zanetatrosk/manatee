1635d215f6bb2821eece578ca18b0f5911d91013
Žaneta Trošková <troskzan@fit.cvut.cz>
2023-10-25 11:45:20 +0200

    wrapping new component

--- src/pages/homePage.tsx
+++ src/pages/homePage.tsx
@@ -1,66 +1,10 @@
-import { Card, Divider, Grid, Typography } from "@mui/material";
 import Box from "@mui/material/Box";
-import React from "react";
 
 export default function HomePage() {
   return (
     <Box display="flex" justifyContent="center">
-      <Card>
-        <Grid container alignItems="center" p={1} columnSpacing={3}>
-          <Grid item p={1}>
-            <Typography variant="h4">Neville Longbottom aaaaa</Typography>
-          </Grid>
-          <Grid item />
-          <Grid item container xs columnSpacing={4}>
-          {data.map((i) => (
-            <React.Fragment>
-              <Grid item/>
-              <Grid
-                item
-                container
-                flexDirection="column"
-                xs
-                wrap="nowrap"
-                p={3}
-                spacing={0.5}
-              >
-                <Grid item xs>
-                  <Typography
-                    component="span"
-                    noWrap
-                    color="text.secondary"
-                    variant="caption"
-                  >
-                    {i.header}
-                  </Typography>
-                </Grid>
-                <Grid item xs>
-                  <Typography
-                   component="span"
-                   noWrap
-                  
-                  >{i.value}</Typography>
-                </Grid>
-              </Grid>
-            </React.Fragment>
-          ))}
-          </Grid>
-        </Grid>
-      </Card>
+
     </Box>
   );
 }
-const data = [
-  {
-    header: "Player",
-    value: "Zanetaaaaaa",
-  },
-  {
-    header: "Race",
-    value: "Human",
-  },
-  {
-    header: "Class & level",
-    value: "Barbarian 1",
-  },
-];
+
