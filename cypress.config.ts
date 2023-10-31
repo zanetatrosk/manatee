6d0b7eb5fc3c24dab6a5e8dafd25a40d69dcdcde
Žaneta Trošková <troskzan@fit.cvut.cz>
2023-10-31 18:05:27 +0100

    removing video flag

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,11 +3,10 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   viewportWidth: 1440,
   viewportHeight: 1080,
-  video: true,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-      
+
     },
   },
 });
