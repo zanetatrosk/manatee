b8dc9f60741694435b1149d399a6b02b9ed2764c
Žaneta Trošková <troskzan@fit.cvut.cz>
2023-10-31 17:54:23 +0100

    editing the cypress config, video=true

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   viewportWidth: 1440,
   viewportHeight: 1080,
+  video: true,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
