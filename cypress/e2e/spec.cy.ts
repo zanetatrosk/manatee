b920daffa51a6c55108c19fec180b04407065ab2
Žaneta Trošková <troskzan@fit.cvut.cz>
2023-10-31 17:36:57 +0100

    adding CI pipeline

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,5 +1,16 @@
-describe('template spec', () => {
-  it('passes', () => {
+
+
+
+describe('visit', () => {
+  before( () => {
     cy.visit('http://localhost:3000/')
-  })
+  } );
+
+  
+  it('get element', () => {
+
+    cy.get('.css-1t6c9ts > [href="/characters"] > .MuiButtonBase-root').click();
+    cy.url().should('include', '/characters')
+  });
+  
 })
