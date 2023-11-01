f5491651aec6ec2d04ed242908551d3c88ec12b6
Žaneta Trošková <troskzan@fit.cvut.cz>
2023-11-01 08:44:25 +0100

    editing test

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,9 +4,10 @@ export default defineConfig({
   viewportWidth: 1440,
   viewportHeight: 1080,
   e2e: {
+    slowTestThreshold: 8000,
     setupNodeEvents(on, config) {
       // implement node event listeners here
-
+      
     },
   },
 });
--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -2,13 +2,9 @@
 
 
 describe('visit', () => {
-  before( () => {
-    cy.visit('http://localhost:3000/')
-  } );
-
   
   it('get element', () => {
-
+    cy.visit('http://localhost:3000/')
     cy.get('.css-1t6c9ts > [href="/characters"] > .MuiButtonBase-root').click();
     cy.url().should('include', '/characters')
   });
