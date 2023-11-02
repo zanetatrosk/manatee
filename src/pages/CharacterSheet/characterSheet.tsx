8e7148a406b9e8dc42fac6f90e5542837bab86f0
Žaneta Trošková <troskzan@fit.cvut.cz>
2023-11-02 08:49:48 +0100

    moving character sheet to different page

--- src/pages/CharacterSheet/characterSheet.tsx
+++ src/pages/CharacterSheet/characterSheet.tsx
@@ -1,322 +1,225 @@
-import React, { useState } from 'react';
-import SkillTable from './components/skillTable';
+import { Card, CardMedia, Container, Grid, Paper } from "@mui/material";
+import HeaderCard from "./components/headerCard";
+import AbilityCard from "./components/abilityCard";
+import SkillTable from "./components/skillTable";
+import StatsGrid from "./components/statsGrid";
+import TabsCard from "./components/tabsCard";
 
-interface CharacterSheetProps {
-    // Define any props that your component needs here
+
+interface Item {
+  header: string;
+  value: string;
 }
 
-const CharacterSheet: React.FC<CharacterSheetProps> = (props) => {
-    // Define any state that your component needs here using useState
-    const [characterName, setCharacterName] = useState('');
-    const [classAndLevel, setClassAndLevel] = useState('');
-    const [background, setBackground] = useState('');
-    const [playerName, setPlayerName] = useState('');
-    const [race, setRace] = useState('');
-    const [alignment, setAlignment] = useState('');
-    const [experiencePoints, setExperiencePoints] = useState(0);
-    const [strength, setStrength] = useState(0);
-    const [dexterity, setDexterity] = useState(0);
-    const [constitution, setConstitution] = useState(0);
-    const [intelligence, setIntelligence] = useState(0);
-    const [wisdom, setWisdom] = useState(0);
-    const [charisma, setCharisma] = useState(0);
-
-    return (
-        <div>
-            <div>
-                <div>
-                    <label>Character Name:</label>
-                    <input type="text" value={characterName} onChange={(e) => setCharacterName(e.target.value)} />
-                </div>
-                <div>
-                    <label>Class and Level:</label>
-                    <input type="text" value={classAndLevel} onChange={(e) => setClassAndLevel(e.target.value)} />
-                </div>
-                <div>
-                    <label>Background:</label>
-                    <input type="text" value={background} onChange={(e) => setBackground(e.target.value)} />
-                </div>
-                <div>
-                    <label>Player Name:</label>
-                    <input type="text" value={playerName} onChange={(e) => setPlayerName(e.target.value)} />
-                </div>
-                <div>
-                    <label>Race:</label>
-                    <input type="text" value={race} onChange={(e) => setRace(e.target.value)} />
-                </div>
-                <div>
-                    <label>Alignment:</label>
-                    <input type="text" value={alignment} onChange={(e) => setAlignment(e.target.value)} />
-                </div>
-                <div>
-                    <label>Experience Points:</label>
-                    <input type="number" value={experiencePoints} onChange={(e) => setExperiencePoints(parseInt(e.target.value))} />
-                </div>
-            </div>
-            <div>
-                <div>
-                    <label>Strength:</label>
-                    <input type="number" value={strength} onChange={(e) => setStrength(parseInt(e.target.value))} />
-                </div>
-                <div>
-                    <label>Dexterity:</label>
-                    <input type="number" value={dexterity} onChange={(e) => setDexterity(parseInt(e.target.value))} />
-                </div>
-                <div>
-                    <label>Constitution:</label>
-                    <input type="number" value={constitution} onChange={(e) => setConstitution(parseInt(e.target.value))} />
-                </div>
-                <div>
-                    <label>Intelligence:</label>
-                    <input type="number" value={intelligence} onChange={(e) => setIntelligence(parseInt(e.target.value))} />
-                </div>
-                <div>
-                    <label>Wisdom:</label>
-                    <input type="number" value={wisdom} onChange={(e) => setWisdom(parseInt(e.target.value))} />
-                </div>
-                <div>
-                    <label>Charisma:</label>
-                    <input type="number" value={charisma} onChange={(e) => setCharisma(parseInt(e.target.value))} />
-                </div>
-            </div>
-            <div>
-                <div>
-                    <label>Inspiration:</label>
-                    <input type="checkbox" />
-                </div>
-                <div>
-                    <label>Proficiency Bonus:</label>
-                    <input type="number" />
-                </div>
-                <div>
-                    <label>Saving Throws:</label>
-                    <input type="text" />
-                </div>
-                <div>
-                    <label>Skills:</label>
-                    <input type="text" />
-                </div>
-                <div>
-                    <label>Passive Wisdom (Perception):</label>
-                    <input type="number" />
-                </div>
-            </div>
-            <div>
-                <div>
-                    <label>Armor Class:</label>
-                    <input type="number" />
-                </div>
-                <div>
-                    <label>Initiative:</label>
-                    <input type="number" />
-                </div>
-                <div>
-                    <label>Speed:</label>
-                    <input type="number" />
-                </div>
-                <div>
-                    <label>Hit Point Maximum:</label>
-                    <input type="number" />
-                </div>
-                <div>
-                    <label>Current Hit Points:</label>
-                    <input type="number" />
-                </div>
-                <div>
-                    <label>Temporary Hit Points:</label>
-                    <input type="number" />
-                </div>
-            </div>
-            <div>
-                <div>
-                    <label>Hit Dice:</label>
-                    <input type="text" />
-                </div>
-                <div>
-                    <label>Death Saves:</label>
-                    <input type="text" />
-                </div>
-            </div>
-            <div>
-                <div>
-                    <label>Personality Traits:</label>
-                    <textarea />
-                </div>
-                <div>
-                    <label>Ideals:</label>
-                    <textarea />
-                </div>
-                <div>
-                    <label>Bonds:</label>
-                    <textarea />
-                </div>
-                <div>
-                    <label>Flaws:</label>
-                    <textarea />
-                </div>
-            </div>
-            <div>
-                <div>
-                    <label>Features & Traits:</label>
-                    <textarea />
-                </div>
-            </div>
-            <div>
-                <div>
-                    <label>Equipment:</label>
-                    <textarea />
-                </div>
-            </div>
-            <div>
-                <div>
-                    <label>Spellcasting Ability:</label>
-                    <input type="text" />
-                </div>
-                <div>
-                    <label>Spell Save DC:</label>
-                    <input type="number" />
-                </div>
-                <div>
-                    <label>Spell Attack Bonus:</label>
-                    <input type="number" />
-                </div>
-            </div>
-            <div>
-                <div>
-                    <label>Spell Slots:</label>
-                    <input type="text" />
-                </div>
-                <div>
-                    <label>1st Level:</label>
-                    <input type="text" />
-                </div>
-                <div>
-                    <label>2nd Level:</label>
-                    <input type="text" />
-                </div>
-                <div>
-                    <label>3rd Level:</label>
-                    <input type="text" />
-                </div>
-                <div>
-                    <label>4th Level:</label>
-                    <input type="text" />
-                </div>
-                <div>
-                    <label>5th Level:</label>
-                    <input type="text" />
-                </div>
-                <div>
-                    <label>6th Level:</label>
-                    <input type="text" />
-                </div>
-                <div>
-                    <label>7th Level:</label>
-                    <input type="text" />
-                </div>
-                <div>
-                    <label>8th Level:</label>
-                    <input type="text" />
-                </div>
-                <div>
-                    <label>9th Level:</label>
-                    <input type="text" />
-                </div>
-            </div>
-            <SkillTable
+export default function CharacterSheet() {
+  return (
+    <Container>
+      <Paper sx={{ p: 2 }} elevation={4}>
+        <Grid container flexDirection={"column"} spacing={5}>
+          {/* first row */}
+          <Grid item container spacing={2}>
+            <Grid item>
+              <Card sx={{ maxWidth: 250, height: "100%", maxHeight: 345 }}>
+                <CardMedia
+                sx={{ height: "100%"}}
+                  component="img"
+                  // Picture by internet user: 
+                  src="https://fiverr-res.cloudinary.com/images/q_auto,f_auto/gigs/318394173/original/c4b3f1c87e9c941530cfdbabda92abbab1b7b622/make-dnd-character-art-and-character-design.png"
+                  title="character"
+                />
+              </Card>
+            </Grid>
+            <Grid item container xs flexDirection={"column"} spacing={3}>
+              <Grid item xs>
+                <HeaderCard />
+              </Grid>
+              <Grid item container spacing={3} justifyContent="center">
+                {Array.from(Array(6).keys()).map((i) => (
+                  <Grid item key={i}>
+                    <AbilityCard ability="Strength" score={12} modifier={1} />
+                  </Grid>
+                ))}
+              </Grid>
+            </Grid>
+          </Grid>
+          {/* second row */}
+          <Grid container item spacing={3} columns={{ xs: 12 }} >
+            <Grid item>
+              <SkillTable
                 name="Skills"
-                description="Please choose skills"
+                description="Choose which skills you are proficient in."
                 tableData={tableData}
-            />
-        </div>
+              />
+            </Grid>
+            <Grid container item flexDirection={"column"} spacing={3} xs>
+              <Grid container item spacing={3}>
+                <Grid item xs>
+                  <SkillTable
+                    name="Saving Throws"
+                    description="Choose which saving throws you are proficient in."
+                    tableData={savingThrows}
+                  />
+                </Grid>
+                <Grid item >
+                  <StatsGrid title="Stats" items={items} />
+                </Grid>
+              </Grid>
+              <Grid container item xs>
+                <Grid item xs>
+                  <TabsCard />
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Paper>
+    </Container>
+  );
+}
 
-    );
-};
 const tableData = [
-    {
-      label: "Animal Handling(WIS)",
-      score: 8,
-      checked: false,
-    },
-    {
-      label: "Arcana(INT)",
-      score: 8,
-      checked: false,
-    },
-    {
-      label: "Athletics(STR)",
-      score: 8,
-      checked: false,
-    },
-    {
-      label: "Deception(CHA)",
-      score: 8,
-      checked: false,
-    },
-    {
-      label: "History(INT)",
-      score: 8,
-      checked: false,
-    },
-    {
-      label: "Insight(WIS)",
-      score: 8,
-      checked: false,
-    },
-    {
-      label: "Intimidation(CHA)",
-      score: 8,
-      checked: false,
-    },
-    {
-      label: "Investigation(INT)",
-      score: 8,
-      checked: false,
-    },
-    {
-      label: "Medicine(WIS)",
-      score: 8,
-      checked: true,
-    },
-    {
-      label: "Nature(INT)",
-      score: 8,
-      checked: false,
-    },
-    {
-      label: "Perception(WIS)",
-      score: 8,
-      checked: false,
-    },
-    {
-      label: "Performance(CHA)",
-      score: 8,
-      checked: false,
-    },
-    {
-      label: "Persuasion(CHA)",
-      score: 8,
-      checked: false,
-    },
-    {
-      label: "Religion(INT)",
-      score: 8,
-      checked: false,
-    },
-    {
-      label: "Sleight of Hand(DEX)",
-      score: 8,
-      checked: false,
-    },
-    {
-      label: "Stealth(DEX)",
-      score: 8,
-      checked: true,
-    },
-    {
-      label: "Survival(WIS)",
-      score: 8,
-      checked: false,
-    },
-  ];
-export default CharacterSheet;
+  {
+    label: "Animal Handling(WIS)",
+    score: 8,
+    checked: false,
+  },
+  {
+    label: "Arcana(INT)",
+    score: 8,
+    checked: true,
+  },
+  {
+    label: "Athletics(STR)",
+    score: 8,
+    checked: false,
+  },
+  {
+    label: "Deception(CHA)",
+    score: 8,
+    checked: false,
+  },
+  {
+    label: "History(INT)",
+    score: 8,
+    checked: false,
+  },
+  {
+    label: "Insight(WIS)",
+    score: 8,
+    checked: false,
+  },
+  {
+    label: "Intimidation(CHA)",
+    score: 8,
+    checked: false,
+  },
+  {
+    label: "Investigation(INT)",
+    score: 8,
+    checked: false,
+  },
+  {
+    label: "Medicine(WIS)",
+    score: 8,
+    checked: true,
+  },
+  {
+    label: "Nature(INT)",
+    score: 8,
+    checked: false,
+  },
+  {
+    label: "Perception(WIS)",
+    score: 8,
+    checked: false,
+  },
+  {
+    label: "Performance(CHA)",
+    score: 8,
+    checked: false,
+  },
+  {
+    label: "Persuasion(CHA)",
+    score: 8,
+    checked: false,
+  },
+  {
+    label: "Religion(INT)",
+    score: 8,
+    checked: false,
+  },
+  {
+    label: "Sleight of Hand(DEX)",
+    score: 8,
+    checked: false,
+  },
+  {
+    label: "Stealth(DEX)",
+    score: 8,
+    checked: true,
+  },
+  {
+    label: "Survival(WIS)",
+    score: 8,
+    checked: false,
+  },
+];
+//generate saving throws in the same format as tableData
+const savingThrows = [
+  {
+    label: "Strength",
+    score: 8,
+    checked: false,
+  },
+  {
+    label: "Dexterity",
+    score: 8,
+    checked: true,
+  },
+  {
+    label: "Constitution",
+    score: 8,
+    checked: false,
+  },
+  {
+    label: "Intelligence",
+    score: 8,
+    checked: false,
+  },
+  {
+    label: "Wisdom",
+    score: 8,
+    checked: false,
+  },
+  {
+    label: "Charisma",
+    score: 8,
+    checked: false,
+  },
+];
+const items: Item[] = [
+  {
+    header: "speed",
+    value: "35 ft",
+  },
+  {
+    header: "Initiative",
+    value: "+2",
+  },
+  {
+    header: "Prof. Bonus",
+    value: "+2",
+  },
+  {
+    header: "Armor Class",
+    value: "15",
+  },
+  {
+    header: "Hit Points max",
+    value: "412",
+  },
+  {
+    header: "Hit Dice",
+    value: "1d10",
+  },
+];
