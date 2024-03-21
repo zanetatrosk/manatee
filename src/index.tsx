568367ee053d7fd3c69fa9cf12f62d7bbd0020d9
Žaneta Trošková <troskzan@fit.cvut.cz>
2024-03-21 15:20:32 +0100

    add service worker registered

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,8 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux'
 import store from './store/store'
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -31,6 +33,9 @@ if (process.env.NODE_ENV === 'development') {
   })
 
 }
+
+serviceWorkerRegistration.register();
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
