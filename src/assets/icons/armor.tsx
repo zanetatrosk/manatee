4d86392962f79d90ef080580ea966d219051b788
Žaneta Trošková <troskzan@fit.cvut.cz>
2024-05-03 22:52:37 +0200

    little refactoring in oth prof. and languages

--- src/assets/icons/armor.tsx
+++ src/assets/icons/armor.tsx
@@ -1,4 +1,4 @@
-export default function Armor() {
+export default function ArmorIcon() {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
       <path
--- src/features/characterTabs/components/otherProficienciesTab.tsx
+++ src/features/characterTabs/components/otherProficienciesTab.tsx
@@ -1,6 +1,6 @@
 import { Grid } from "@mui/material";
 import ProficienciesCard from "./proficienciesCard";
-import Armor from "@assets/icons/armor";
+import ArmorIcon from "@assets/icons/armor";
 import WeaponIcon from "@assets/icons/weapon";
 import ToolsIcon from "@assets/icons/tools";
 import LanguagesIcon from "@assets/icons/languages";
@@ -11,34 +11,30 @@ export default function OtherProficienciesTab() {
   const { languages, tools, armor, weapons } = useAppSelector(
     (state) => state.character.proficiencies,
   );
+  const join = (arr: string[]) => arr.join(", ") || OTH_PROFICIENCIES.NO_DATA;
   const OTH_PROFICIENCIES = CHARACTER_SHEET.OTH_PROFICIENCIES;
   const data = [
     {
       title: OTH_PROFICIENCIES.ARMOR,
-      data: OTH_PROFICIENCIES.NO_DATA,
-      icon: <Armor />,
+      data: join(languages),
+      icon: <ArmorIcon />,
     },
     {
       title: OTH_PROFICIENCIES.WEAPONS,
-      data: OTH_PROFICIENCIES.NO_DATA,
+      data: join(weapons),
       icon: <WeaponIcon />,
     },
     {
       title: OTH_PROFICIENCIES.TOOLS,
-      data: OTH_PROFICIENCIES.NO_DATA,
+      data: join(tools),
       icon: <ToolsIcon />,
     },
     {
       title: OTH_PROFICIENCIES.LANGUAGES,
-      data: OTH_PROFICIENCIES.NO_DATA,
+      data: join(armor),
       icon: <LanguagesIcon />,
     },
   ];
-
-  if (languages.length > 0) data[3].data = languages.join(", ");
-  if (tools.length > 0) data[2].data = tools.join(", ");
-  if (armor) data[0].data = armor.join(", ");
-  if (weapons.length > 0) data[1].data = weapons.join(", ");
 
   return (
     <>
