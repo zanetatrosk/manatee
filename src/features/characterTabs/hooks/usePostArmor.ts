e06902f077a1489b5b3dffe667796e4be702b754
Žaneta Trošková <troskzan@fit.cvut.cz>
2024-05-06 16:08:57 +0200

    fix bug with armor

--- src/features/characterTabs/hooks/usePostArmor.ts
+++ src/features/characterTabs/hooks/usePostArmor.ts
@@ -3,6 +3,7 @@ import { usePostArmorByCharacterIdMutation } from "api/charactersApiSlice";
 const usePostArmor = () => {
     const [postArmorByCharacterId] = usePostArmorByCharacterIdMutation();
     const postArmor = (armor: string[], id: string) => {
+        if(armor.length === 0) return;
         if (id) {
             postArmorByCharacterId({ id, armor: armor[0] });
         }
